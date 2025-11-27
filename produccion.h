6466bc91c5b0780b5cd8095e5fe40879c7535d20
Dielol047 <diego.erazo.lascano@udla.edu.ec>
2025-11-27 15:11:54 -0500

    Add resource management and shopping cart functions
    Added functions to manage available resources and shopping cart.

--- produccion.h
+++ produccion.h
@@ -7,14 +7,16 @@
 #define MAX_NAME_LENGTH 50
 
 
-void ingresarDatos(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int demandas[], int* cantidad_productos);
-void mostrarProductos(const char nombres[][MAX_NAME_LENGTH], const float tiempos[], const float recursos[], const float cobre[], const int demandas[], int cantidad_productos);
-void editarProducto(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int demandas[], int cantidad_productos);
-void eliminarProducto(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int demandas[], int* cantidad_productos);
-void calcularTotales(const char nombres[][MAX_NAME_LENGTH], const float tiempos[], const float recursos[], const float cobre[], const int demandas[], int cantidad_productos);
-void verificarViabilidad(const char nombres[][MAX_NAME_LENGTH], const float tiempos[], const float recursos[], const float cobre[], const int demandas[], int cantidad_productos);
-int buscarProducto(const char nombres[][MAX_NAME_LENGTH], int cantidad_productos, const char* nombre_buscar);
+void ingresarDatos(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int* cantidad_productos);
+void mostrarProductos(const char nombres[][MAX_NAME_LENGTH], const float tiempos[], const float recursos[], const float cobre[], int cantidad_productos);
+void editarProducto(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int cantidad_productos);
+void eliminarProducto(char nombres[][MAX_NAME_LENGTH], float tiempos[], float recursos[], float cobre[], int* cantidad_productos);
 
+
+void ingresarRecursosDisponibles(float* tiempo_disp, float* alum_disp, float* cobre_disp);
+void gestionarCarrito(const char nombres[][MAX_NAME_LENGTH], const float tiempos[], const float recursos[], const float cobre[], int cantidad_productos, float* tiempo_disp, float* alum_disp, float* cobre_disp);
+
+int buscarProducto(const char nombres[][MAX_NAME_LENGTH], int cantidad_productos, const char* nombre_buscar);
 
 float ingresaFloat(const char* mensaje);
 int ingresaEntero(const char* mensaje);
